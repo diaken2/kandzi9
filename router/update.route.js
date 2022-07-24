ee563e696639ae5f124f086bdc7f4c7785556b34
Кенан Салахоа <you@example.com>
2022-07-25 01:17:31 +0500

    kandzi5

--- router/update.route.js
+++ router/update.route.js
@@ -12,21 +12,88 @@ router.post("/temme",async(req,res)=>{
     
 })
 
+
+
+router.post("/kandzichange",async(req,res)=>{
+console.log(req.body)
+
+Todo.updateOne({_id:req.body.idfor}, {kandziword:{
+  kandzi:req.body.kandzifor,
+  read:req.body.readorig,
+  translate:req.body.transorig
+}},()=>{
+  console.log("Изменено")
+});
+})
+router.post("/readchange",async(req,res)=>{
+  console.log(req.body)
+  Todo.updateOne({_id:req.body.idfor}, {kandziword:{
+    kandzi:req.body.kandzifor,
+    read:req.body.readorig,
+    translate:req.body.transorig
+  }},()=>{
+    console.log("Изменено")
+  })
+})
+
+
+
+router.post("/imagechange",fileMiddleware.single('avatar'),(req,res)=>{
+  console.log("Подготовка к изменениям")
+  console.log(req.body)
+  console.log(req.file)
+  imgbbUploader("5e71f03e6521ab68c9a55e6b59526aa3", req.file.path)
+  .then((response) => {
+    console.log(response.url)
+    Todo.updateOne({_id:req.body.idfor}, {photo:response.url},()=>{
+      console.log("Изменено")
+    })
+  })
+  .catch((error) => console.error(error));
+  res.json(req.body)
+})
+
+
+
+
+
+router.post("/translatechange",async(req,res)=>{
+  console.log(req.body)
+  Todo.updateOne({_id:req.body.idfor}, {kandziword:{
+    kandzi:req.body.kandzifor,
+    read:req.body.readorig,
+    translate:req.body.transorig
+  }},()=>{
+    console.log("Изменено")
+  })
+  res.json(req.body)
+})
 // router.post('/create2',fileMiddleware.single('avatar'), (req,res)=>{
 //     console.log("Файл", req.file)
 //     res.json(req.file)
 // })
+
+router.post('/delete', (req,res)=>{
+  console.log("Файл удален")
+  console.log(req.body)
+  Todo.findOneAndDelete({_id:req.body.idfor},()=>{
+    console.log("Всё удалилось")
+  })
+  res.json(req.body)
+})
+
+
+
 router.post('/create',fileMiddleware.single('avatar'),(req,res)=>{
 console.log("Подключение к серверу прошло успешно")
 console.log(req.body)
 console.log(req.file)
 
-/* or use import in ESM projects:
-import { imgbbUploader } from "imgbb-uploader"; 
-*/
+
 
 imgbbUploader("5e71f03e6521ab68c9a55e6b59526aa3", req.file.path)
   .then(async(response) => {
+    console.log(response.url)
     const todo = new Todo({
         kandziword:req.body,
         photo:response.url
