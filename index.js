283bb2f23aebbc8bedc2f42e81a0c1220857b87b
diaken2 <107970623+diaken2@users.noreply.github.com>
2022-10-08 02:50:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json({extended:true}))
 app.use("/api", routes)
 app.use(express.urlencoded({extended:false}))
 app.use('/images',express.static(path.join(__dirname,"images")))
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 const PORT=process.env.PORT || 5000
 const start=async()=>{
     try{
